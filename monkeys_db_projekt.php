b051503f228c41204d8c6b5c2f7a7f5f7e81308f
goofyx <k.faryna@goofyx.pl>
2017-10-09 13:32:38 +0200

    git #14 pliki globlne z konfiguracjami

---
+++ monkeys_db_projekt.php
@@ -0,0 +1,8 @@
+<?php
+
+	$db_projekt_serwer = "127.0.0.1";
+	$db_projekt_user = "boincadm";
+	$db_projekt_haslo = "boincadm_Haslo123";
+	$db_projekt_baza = "goofyx_grid_nci";
+
+?>
---
+++ monkeys_katalogi.php
@@ -0,0 +1,10 @@
+<?php
+
+	$katalog_domowy = "/home/boincadm/goofyx_grid_nci";
+	$katalog_pid = $katalog_domowy."/pid_ggNCI/";
+	$katalog_srAnalyzed = $katalog_domowy."/sample_results_analyzed";
+	$katalog_sr = $katalog_domowy."/sample_results";
+ 	$katalog_srArchiwum = $katalog_domowy."/dane_goofyx_grid";
+    $katalog_templates = "/home/boincadm/goofyx_grid_nci/templates";
+    $katalog_download_temp = "/home/boincadm/goofyx_grid_nci/download_temp";
+?>
