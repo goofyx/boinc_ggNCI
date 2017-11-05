f08e75433aa105bb1ab5fd530fd45c9d180f0857
goofyx <k.faryna@goofyx.pl>
2017-11-05 22:14:01 +0100

    git #46 skrypty z xu4 w osobnym folderze

---
+++ skrypty_XU4/monkeys_katalogi.php
@@ -0,0 +1,9 @@
+<?php
+
+	$katalog_domowy = "/home/goofyx/ggNCI";
+	$katalog_srAnalyzed = $katalog_domowy."/sample_results_analyzed";
+	$katalog_srNoUser = $katalog_domowy."/sample_results_noUser";
+	$katalog_sr = $katalog_domowy."/sample_results_doAnalizy";
+	$katalog_archives_results = $katalog_domowy."/archives_results";
+	
+?>
---
+++ skrypty_XU4/monkeys_vX_sorting_results.php
@@ -0,0 +1,59 @@
+	<?php
+
+  echo "START MONKEYS_SORTING_FILES\n";
+  
+   $plik_lock = "/home/boincadm/goofyx_grid_nci/pid_ggNCI/monkeys_vX_sorting_files.lock";
+  if (file_exists($plik_lock)){
+   exit;   
+  }
+  
+  include( "monkeys_katalogi.php" );  
+  
+  $plik = file_put_contents( $plik_lock, "LOCK" );
+  
+  $katalog_zrodlowy = $katalog_srAnalyzed;
+  $katalog_docelowy = $katalog_archives_results;
+  
+  $licznik = 0;
+  foreach( new directoryiterator( $katalog_zrodlowy ) as $plik )
+  {
+   if(!$plik->isDot())
+   {
+     if (filesize($katalog_zrodlowy."/".$plik) > 0 )
+     {   
+       $skladowe_nazwy = explode( "_", $plik->getFileName() );    
+       $skladowe_serii = explode( "-", $skladowe_nazwy[3] );    
+     //  $katalog_pliku = $katalog_docelowy."/".date('Y_m_d')."_".$skladowe_nazwy[0]."/".$skladowe_nazwy[1]."/".$skladowe_serii[1]."/".$skladowe_serii[0];
+       $katalog_pliku = $katalog_docelowy."/".$skladowe_nazwy[0]."_".date('Y_m_d')."/".$skladowe_nazwy[1]."/".$skladowe_serii[1]."/".$skladowe_serii[0];
+       if (!file_exists($katalog_pliku)){
+	if (!mkdir( $katalog_pliku, 0777, true )){
+	    echo "Błąd tworzenia katalogu: ".$katalog_pliku."\n";
+	}
+       }
+       
+       if (file_exists( $katalog_pliku."/".$plik->getFileName() ) ){
+         unlink( $katalog_zrodlowy."/".$plik );
+       } else {
+       
+         if (!copy($katalog_zrodlowy."/".$plik, $katalog_pliku."/".$plik->getFileName() ) )
+         {       
+	      echo "Błąd kopiowania/przenoszenia pliku: ".plik." do: ".$katalog_pliku."/".$plik->getFileName()."\n";
+         } else {        
+            $licznik++;
+			unlink($katalog_zrodlowy."/".basename($plik));
+	//     echo "Przeniesiono plik nr.: ".$licznik." o nazwie: ".$plik."\n";
+         }
+       }
+     } else {
+       unlink($katalog_zrodlowy."/".$plik);
+  //      echo "Skasowano pusty plik: ".$plik."\n";
+     }
+   }  
+  }
+  
+  echo "plików: ".$licznik."\n";
+  unlink($plik_lock);
+  
+  echo "KONIEC MONKEYS_SORTING_FILES\n";
+
+?>
