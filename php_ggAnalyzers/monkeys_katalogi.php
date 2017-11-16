cd5672728bdacd3a1cfb797b8e2d1e5a3b75ec43
goofyx <k.faryna@goofyx.pl>
2017-11-16 13:40:54 +0100

    #61 cleaning up repository

---
+++ php_ggAnalyzers/monkeys_katalogi.php
@@ -0,0 +1,14 @@
+<?php
+
+	$katalog_domowy_dane = "/home/goofyx/ggNCI";
+	$katalog_domowy_skrypty = "/home/goofyx/ggNCI";
+	
+	$katalog_srAnalyzed = $katalog_domowy_dane."/sample_results_analyzed";
+	$katalog_srNoUser = $katalog_domowy_dane."/sample_results_noUser";
+	
+	$katalog_sr = $katalog_domowy_dane."/sample_results_doAnalizy";
+	$katalog_sr_NAS4 = $katalog_domowy_dane."/sample_results_doAnalizy_NAS4";
+	
+	$katalog_archives_results_NAS4 = $katalog_domowy_dane."/archives_results_NAS4";
+	
+?>
