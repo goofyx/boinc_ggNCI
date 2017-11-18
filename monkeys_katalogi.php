da3a71d19295433802d8155c891459b7e3072c5c
goofyx <k.faryna@goofyx.pl>
2017-11-18 22:18:43 +0100

    #4 fix foldr in variable

--- monkeys_katalogi.php
+++ monkeys_katalogi.php
@@ -1,14 +1,12 @@
 <?php
 
-	$katalog_domowy_dane = "/home/goofyx/ggNCI";
-	$katalog_domowy_skrypty = "/home/goofyx/ggNCI";
-	
-	$katalog_srAnalyzed = $katalog_domowy_dane."/sample_results_analyzed";
-	$katalog_srNoUser = $katalog_domowy_dane."/sample_results_noUser";
-	
-	$katalog_sr = $katalog_domowy_dane."/sample_results_doAnalizy";
-	$katalog_sr_NAS4 = $katalog_domowy_dane."/sample_results_doAnalizy_NAS4";
-	
-	$katalog_archives_results_NAS4 = $katalog_domowy_dane."/archives_results_NAS4";
-	
+	$katalog_domowy = "/home/boincadm/goofyx_grid_nci";
+	$katalog_pid = $katalog_domowy."/pid_ggNCI";
+	$katalog_srAnalyzed = $katalog_domowy."/sample_results_analyzed";
+	$katalog_sr = $katalog_domowy."/sample_results";
+ 	$katalog_srArchiwum = $katalog_domowy."/dane_goofyx_grid";
+    $katalog_templates = "/home/boincadm/goofyx_grid_nci/templates";
+    $katalog_download_temp = "/home/boincadm/goofyx_grid_nci/download_temp";
+	$katalog_archives_results = $katalog_domowy."/archives_results";
+	$katalog_doAnalizy = $katalog_domowy."/sample_results_doAnalizy";
 ?>
